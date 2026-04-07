5654ca49a997b1fa1d3d0c77ff429e897ac66c8c
Gideon Ibitoye <ibitoyrgideon79@gmail.com>
2026-04-07 16:18:07 +0100

    Wire auth and wallet API routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
 
 import { AuthController } from "../controllers/auth.controller";
+import { AuthRepository } from "../repositories/auth.repository";
+import { UserRepository } from "../repositories/user.repository";
+import { WalletRepository } from "../repositories/wallet.repository";
+import { AuthService } from "../services/auth.service";
+import { BlacklistService } from "../services/blacklist.service";
+import { UserService } from "../services/user.service";
 
 const authRouter = Router();
-const authController = new AuthController();
+const userRepository = new UserRepository();
+const walletRepository = new WalletRepository();
+const authRepository = new AuthRepository();
+const blacklistService = new BlacklistService();
+const userService = new UserService(userRepository, walletRepository, blacklistService);
+const authService = new AuthService(userRepository, authRepository);
+const authController = new AuthController(userService, authService);
 
-authRouter.post("/register", (request, response) => authController.register(request, response));
-authRouter.post("/login", (request, response) => authController.login(request, response));
+authRouter.post("/register", async (request, response) => authController.register(request, response));
+authRouter.post("/login", async (request, response) => authController.login(request, response));
 
 export default authRouter;
--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from "express";
 
 import { WalletController } from "../controllers/wallet.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { AuthRepository } from "../repositories/auth.repository";
+import { UserRepository } from "../repositories/user.repository";
+import { WalletRepository } from "../repositories/wallet.repository";
+import { AuthService } from "../services/auth.service";
+import { WalletService } from "../services/wallet.service";
 
 const walletRouter = Router();
-const walletController = new WalletController();
+const userRepository = new UserRepository();
+const authRepository = new AuthRepository();
+const walletRepository = new WalletRepository();
+const authService = new AuthService(userRepository, authRepository);
+const walletService = new WalletService(walletRepository);
+const walletController = new WalletController(walletService);
 
-walletRouter.get("/me", (request, response) => walletController.getWallet(request, response));
-walletRouter.post("/fund", (request, response) => walletController.fundWallet(request, response));
-walletRouter.post("/transfer", (request, response) => walletController.transferFunds(request, response));
-walletRouter.post("/withdraw", (request, response) => walletController.withdrawFunds(request, response));
+walletRouter.use(authMiddleware(authService));
+
+walletRouter.get("/me", async (request, response) => walletController.getWallet(request, response));
+walletRouter.get("/transactions", async (request, response) => walletController.getTransactions(request, response));
+walletRouter.post("/fund", async (request, response) => walletController.fundWallet(request, response));
+walletRouter.post("/transfer", async (request, response) => walletController.transferFunds(request, response));
+walletRouter.post("/withdraw", async (request, response) => walletController.withdrawFunds(request, response));
 
 export default walletRouter;
